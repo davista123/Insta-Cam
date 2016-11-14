b48ddcfff921fa256580946fdcb8b5f07a0c5304
David Uche <uchedavy@gmail.com>
2016-11-14 07:23:01 -0600

    Added __init__.py

--- imcap.py
+++ imcap.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np 
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 vid = cv2.VideoCapture(0)
 while(True):
 	_, frame = vid.read()
 	cv2.imshow('frame', frame)
-	font = cv2.FONT_HERSHEY_SIMPLEX
-	cv2.putText(frame,'Press (s) to browse image',(10,500), font, 1, (200,255,155), 2, cv2.LINE_AA)
+	cv2.putText(frame,'Hello World!',(10,500), font, 1,(255,255,255),2)
 	if cv2.waitKey(1) & 0xFF == ord('s'):
 		cv2.imwrite("frame.jpg",frame)
 		break
