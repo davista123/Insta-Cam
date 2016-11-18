f1ea99c9fcef75a5fae1414c00d776412aa039df
David Uche <uchedavy@gmail.com>
2016-11-18 11:03:10 -0600

    Basic Program implementation

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from keras.preprocessing import image
 import numpy as np
 import json
 import webbrowser
-import firebase
+import cv2
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+vid = cv2.VideoCapture(0)
+while(True):
+	_, frame = vid.read()
+	cv2.imshow('frame', frame)
+	cv2.putText(frame,'Hello World!',(10,500), font, 1,(255,255,255),2)
+	if cv2.waitKey(1) & 0xFF == ord('s'):
+		cv2.imwrite("frame.jpg",frame)
+		break
+vid.release()
+cv2.destroyAllWindows()
 
 
 #The default input size for this model is 224x224.
